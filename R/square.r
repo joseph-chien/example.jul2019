614aa2c3c316c7b4749480e3637bb9f70e95d7c7
Joseph-Chien <joseph.chien@live.com>
2019-07-03 16:33:15 +1000

    Initial commit

---
+++ R/square.r
@@ -0,0 +1,46 @@
+#' Square
+#'
+#' Sqare the input
+#' 
+#'
+#' @param x Input numerical vector
+#'
+#' @return Numerical vector
+#' @export
+#'
+#' @examples
+square <- function (x) {
+  x^2 
+}
+
+
+#' cube
+#'
+#' cube the input
+#' 
+#'
+#' @param x Input numerical vector
+#'
+#' @return Numerical vector
+#' @export
+#'
+#' @examples
+cube <- function (x) {
+  x^3 
+}
+
+
+
+#' Odds
+#'
+#' @param x 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+is.odd <- function(x) {
+  gtools::odd(x) 
+}
+
+
